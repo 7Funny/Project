0eba452b6785e46344b03f75cd1547fd9a333a50
Bomterry <73750577+7Funny@users.noreply.github.com>
2022-02-06 15:26:00 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Project
+{
+    class Person
+    {
+        public string Surname { get; }
+        public string Name { get; }
+        public string Patronomyc { get; }
+        public bool Gender { get; }
+        public float Height { get; }
+        public DateTime Birthday { get; }
+        public int Age
+        {
+            get
+            {
+                DateTime temp = new DateTime(Birthday.Year, DateTime.Now.Month, DateTime.Now.Day);
+                return Birthday.CompareTo(temp) > 0 ? DateTime.Now.Year - Birthday.Year - 1 : DateTime.Now.Year - Birthday.Year;
+            }
+        }
+
+        public Person(string surname, string name, string patronomyc, bool gender, float height, DateTime birthday)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Patronomyc = patronomyc;
+            this.Gender = gender;
+            this.Height = height;
+            this.Birthday = birthday;
+        }
+        public static Person Parse(string line)
+        {
+            string[] elem = line.Split(',');
+            Person person = new Person(
+                     elem[0], elem[1], elem[2],
+                     elem[3] == "Male" ? true : false,
+                     float.Parse(elem[4]),
+                     DateTime.Parse(elem[5])
+                     );
+            return person;
+        }
+        public override string ToString()
+        {
+            string result = $"{this.Surname} {this.Name} {this.Patronomyc}, {(this.Gender ? "Male" : "Female")}, {this.Height:F2}, {this.Birthday.ToString(@"dd MMM yyyy")}, {this.Age} y.o.";
+            return result;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            foreach (string line in File.ReadAllLines("persons.txt"))
+            {
+                persons.Add(Person.Parse(line));
+            }
+
+            bool trueNumber = true;
+
+            while (trueNumber)
+            {
+                Console.WriteLine("Enter command number:");
+                Console.WriteLine("1. Sort ascending?");
+                Console.WriteLine("2. Sort descending?");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        persons.Sort((x, y) => x.Birthday.CompareTo(y.Birthday));
+                        trueNumber = false;
+                        break;
+                    case 2:
+                        persons.Sort((x, y) => y.Birthday.CompareTo(x.Birthday));
+                        trueNumber = false;
+                        break;
+                    default:
+                        Console.WriteLine(">> Wrong command number!");
+                        break;
+                }
+            }
+            foreach(var person in persons)
+                File.AppendAllText("out.txt", $"{person.ToString() +"\n"}");
+        }       
+    }
+}
